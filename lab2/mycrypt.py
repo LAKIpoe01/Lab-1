95d1ad924f55e3c972e6a28f6fda5a56640f49d1
LAKIpoe01 <laki.poe@gmail.com>
2024-10-08 17:59:12 +0300

    ok

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -3,16 +3,18 @@ import codecs
 def encode(s):
     if not isinstance(s,str):
         raise TypeError
-    if not s:
-        raise ValueError
     origlen = len(s)
     crypted = ""
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+
     if len(s) > 1000:
         raise ValueError
     s = s.ljust(1000, "a")
+    
     for c in s:
-        if c.isalpha() and ord(c) <= ord('z'):
+        if c.isalpha():
+            if ord(c) > 127:
+                raise ValueError
             if c.islower():
                 c=c.upper()
             # Rot13 the character for maximum security
